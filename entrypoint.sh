66c997dd63e12f78889cdbe3add6d159c8230cbe
sabjorn <info@sabjorn.net>
2023-10-05 15:52:58 -0700

    fix issue with entrypoint preventing hou module import in python

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
-set -e
 
 export PYTHONPATH=/opt/houdini/build/houdini/python3.9libs
 export HOUDINI_SCRIPT_LICENSE="hbatch"
 export HOUDINI_DISABLE_JEMALLOCTEST=1
 
-echo "starting hserver"
-hserver 
-hserver -C -S https://www.sidefx.com/license/sesinetd --clientid $SIDEFX_CLIENT --clientsecret $SIDEFX_SECRET
+function start_hserver {
+  hserver > /dev/null 2>&1
+  hserver -C -S https://www.sidefx.com/license/sesinetd --clientid $SIDEFX_CLIENT --clientsecret $SIDEFX_SECRET > /dev/null 2>&1
+  sesictrl login --email $HOUDINI_USERNAME --password $HOUDINI_PASSWORD > /dev/null 2>&1
+}
 
-echo "logging in"
-echo ${SIDEFX_CLIENT} ${SIDEFX_SECRET} > /secret
-echo "HOUDINI_API_KEY_FILE=/secret" >> /root/houdini19.5/houdini.env
-sesictrl login --email $HOUDINI_USERNAME --password $HOUDINI_PASSWORD 
+echo "starting hserver -- this can take a moment"
+start_hserver
+
+counter=0
+max_attempts=10
+while true; do
+  python -c "import hou"
+  if [ $? -eq 0 ]; then
+    break
+  fi
+  
+  hserver -q > /dev/null 2>&1
+  start_hserver
+  ((counter++))
+  if [[ $counter -gt $max_attempts ]]; then
+    echo "Maximum number of attempts reached. Exiting..."
+    exit 1
+  fi
+done
 
 exec "$@"
+
