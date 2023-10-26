a9c4ea746253871589613103936cde7651b8cf43
Steeve A. Bjornson <info@sabjorn.net>
2023-10-26 10:38:20 -0700

    add function to entrypoint to force relinquish licenses when 'FORCE_LICENSE_RELINQUISH' set

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,32 @@ export PYTHONPATH=/opt/houdini/build/houdini/python3.9libs
 export HOUDINI_SCRIPT_LICENSE="hbatch -R"
 export HOUDINI_DISABLE_JEMALLOCTEST=1
 
-echo "starting hserver -- this can take a moment"
-hserver > /dev/null 2>&1
-hserver -C -S https://www.sidefx.com/license/sesinetd --clientid $SIDEFX_CLIENT --clientsecret $SIDEFX_SECRET > /dev/null 2>&1
-sleep 1 # attempt to give the server time to settle
+get_and_relinquish_users() {
+  # Run the CLI command and capture the output
+  local output=$(sesictrl print-license --format json)
 
-echo "logging in" # "succeed" but import hou can still fail :(
-sesictrl login --email $HOUDINI_USERNAME --password $HOUDINI_PASSWORD
-sleep 1 # attempt to give the server time to settle
+  # Parse the JSON to get 'users->id' where 'product' starts with 'Houdini-Engine'
+  local user_ids=$(echo "$output" | jq -r '.licenses[] | select(.product | startswith("Houdini-Engine")) | .users[].id')
+
+  # Check if any IDs were found
+  if [[ -z "$user_ids" ]]; then
+    return 1
+  fi
+
+  # Relinquish each found user ID
+  for id in $user_ids; do
+    sesictrl relinquish "$id"
+  done
+}
+
+echo "starting hserver -- this can take a moment"
+hserver --render-only -C -S https://www.sidefx.com/license/sesinetd --clientid $SIDEFX_CLIENT --clientsecret $SIDEFX_SECRET > /dev/null 2>&1
+sesictrl login --email $HOUDINI_USERNAME --password $HOUDINI_PASSWORD > /dev/null 2>&1
+
+if [[ ! -z "${FORCE_LICENSE_RELINQUISH}" ]]; then
+  echo "attempting to force removal of license(s)"
+  get_and_relinquish_users
+fi
 
 # test if works
 python -c "import hou" > /dev/null 2>&1
@@ -18,7 +36,7 @@ exit_code=$?
 
 # Check if the exit code is non-zero
 if [ $exit_code -ne 0 ]; then
-  echo "server failed to properly initialize. restart the container and try again"
+  echo "'import hou' failed. This is likely caused by no licenses being available. Try rerunning with 'FORCE_LICENSE_RELINQUISH=1'"
   exit $exit_code
 fi
 
