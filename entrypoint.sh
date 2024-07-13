11a10ced350b88513f55d12160bad2c99bd09f26
sabjorn <info@sabjorn.net>
2024-07-12 17:18:00 -0700

    Squashed commit of the following:
    commit a192161b40e1cb627885c649664bc59e112f71f2
    Author: sabjorn <info@sabjorn.net>
    Date:   Fri Jul 12 16:25:36 2024 -0700
    
        add 'VERBOSE' ENV to allow users to see output of houdini server processes for debugging
    
    commit 702ffe6ef1ce49ea691751e700739e197bd28e86
    Author: sabjorn <info@sabjorn.net>
    Date:   Fri Jul 12 16:24:41 2024 -0700
    
        add 'HOUDINI_LICENSE_MODE' ENV to allow users to set the license mode for loading HOU

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,68 +4,52 @@ export PYTHONPATH=/opt/houdini/build/houdini/python3.9libs
 export HOUDINI_SCRIPT_LICENSE="hbatch -R"
 export HOUDINI_DISABLE_JEMALLOCTEST=1
 
-check_houdini_license_available() {
-  # Execute the CLI command and store its output in a variable
-  local json_output
-  json_output=$(sesictrl print-license --format json)
-
-  # If the command fails, return 0
-  if [ $? -ne 0 ]; then
-    echo 0
-    return
-  fi
-
-  # Use jq to sum the 'available' fields for licenses where "product" starts with "Houdini-Engine"
-  local total_available
-  total_available=$(echo "$json_output" | jq '[.licenses[] | select(.product | startswith("Houdini-Engine"))] | map(.available) | add // 0')
-
-  # Output the total number of available licenses
-  echo "$total_available"
-}
-
-get_and_relinquish_users() {
-  # Run the CLI command and capture the output
-  local output=$(sesictrl print-license --format json)
-
-  # Parse the JSON to get 'users->id' where 'product' starts with 'Houdini-Engine'
-  local user_ids=$(echo "$output" | jq -r '.licenses[] | select(.product | startswith("Houdini-Engine")) | .users[].id')
-
-  # Check if any IDs were found
-  if [[ -z "$user_ids" ]]; then
-    return 1
-  fi
-
-  # Relinquish each found user ID
-  for id in $user_ids; do
-    sesictrl relinquish "$id"
-  done
-}
-
-echo "starting hserver -- this can take a moment"
-hserver -Q --render-only -S https://www.sidefx.com/license/sesinetd --clientid $SIDEFX_CLIENT --clientsecret $SIDEFX_SECRET > /dev/null 2>&1
-sesictrl login --email $HOUDINI_USERNAME --password $HOUDINI_PASSWORD > /dev/null 2>&1
-
-available_licenses=$(check_houdini_license_available)
-if [ "$available_licenses" -eq 0 ]; then
-  echo "No Houdini Engine licenses available."
-  
-  if [ -n "$FORCE_LICENSE_RELINQUISH" ]; then
-    echo "Attempting to force removal of license(s)."
-    get_and_relinquish_users
-  else
-    echo "Cannot proceed--try re-running with 'FORCE_LICENSE_RELINQUISH=1'."
-  fi
+if [ "$VERBOSE" = "true" ]; then
+  echo "VERBOSE mode set"
+  REDIRECT=""
+else
+  REDIRECT="> /dev/null 2>&1"
 fi
 
+if [ -z "$HOUDINI_LICENSE_MODE" ]; then
+  echo "HOUDINI_LICENSE_MODE is not set. Defaulting to 'commercial'."
+  HOUDINI_LICENSE_MODE="commercial"
+else
+  HOUDINI_LICENSE_MODE="${HOUDINI_LICENSE_MODE,,}"  # Convert to lowercase
+fi
+
+case "$HOUDINI_LICENSE_MODE" in
+  "commercial")
+    export HOUDINI_HYTHON_LIC_OPT="--check-licenses=Houdini-Master --skip-licenses=Houdini-Escape,Houdini-Engine"
+    ;;
+  "education")
+    export HOUDINI_HYTHON_LIC_OPT="--skip-license-modes=commercial,apprentice,indie"
+    ;;
+  "indie")
+    export HOUDINI_HYTHON_LIC_OPT="--skip-license-modes=commercial,education,apprentice"
+    ;;
+  "apprentice")
+    export HOUDINI_HYTHON_LIC_OPT="--skip-license-modes=commercial,education,indie"
+    ;;
+  *)
+    echo "Invalid HOUDINI_LICENSE_MODE: $HOUDINI_LICENSE_MODE"
+    echo "Try 'commercial', 'education', 'indie', or 'apprentice'"
+    exit 1
+    ;;
+esac
+
+echo "starting hserver -- this can take a moment"
+eval "hserver -Q --render-only -S https://www.sidefx.com/license/sesinetd --clientid \$SIDEFX_CLIENT --clientsecret \$SIDEFX_SECRET $REDIRECT"
+eval "sesictrl login --email \$HOUDINI_USERNAME --password \$HOUDINI_PASSWORD $REDIRECT"
+
 # test if hou can be imported -- if fails, there is an issue
-python -c "import hou" > /dev/null 2>&1
+eval "python -c 'import hou' $REDIRECT"
 exit_code=$?
 
 # Check if the exit code is non-zero
 if [ $exit_code -ne 0 ]; then
-  echo "'import hou' failed. This is likely caused by no licenses being available. Try rerunning with 'FORCE_LICENSE_RELINQUISH=1'"
+  echo "'import hou' failed. This is likely caused by no licenses being available. Try setting 'HOUDINI_LICENSE_MODE' to a different mode."
   exit $exit_code
 fi
 
 exec "$@"
-
